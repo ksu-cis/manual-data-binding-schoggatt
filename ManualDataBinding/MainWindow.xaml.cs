0de27d133070feeaadee0f584079309ca5fc8eef
Sam Hoggatt <schoggatt@ksu.edu>
2020-03-23 13:58:47 -0500

    DONE.

--- ManualDataBinding/MainWindow.xaml.cs
+++ ManualDataBinding/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ManualDataBinding.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,47 @@ namespace ManualDataBinding
     /// </summary>
     public partial class MainWindow : Window
     {
+        Note note = new Note();
+
         public MainWindow()
         {
             InitializeComponent();
+            Editor.Note = note;
+            ClearNote.Click += OnClearNote;
+            NewNote.Click += OnNewNote;
+            MutateNote.Click += OnMutateNote;
+        }
+
+        /// <summary>
+        /// Event handler to create a new note and apply it to the editor
+        /// </summary>
+        /// <param name="sender">The button clicked</param>
+        /// <param name="e">The event arguments</param>
+        public void OnNewNote(object sender, RoutedEventArgs e)
+        {
+            note = new Note();
+            Editor.Note = note;
+        }
+
+        /// <summary>
+        /// Event handler to clear (erase) the text of the current note
+        /// </summary>
+        /// <param name="sender">The button clicked</param>
+        /// <param name="e">The event arguements</param>
+        public void OnClearNote(object sender, RoutedEventArgs e)
+        {
+            note.Body = "";
+        }
+
+        /// <summary>
+        /// Event handler to mutate the current note
+        /// </summary>
+        /// <param name="sender">The button clicked</param>
+        /// <param name="e">The event arguements</param>
+        public void OnMutateNote(object sender, RoutedEventArgs e)
+        {
+            note.Title = "Master Splinter:";
+            note.Body = "There is no monster more dangerous than a lack of compassion.";
         }
     }
 }
